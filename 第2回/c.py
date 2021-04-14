1207e25730e4f4261e6765e27d63b0303b225f74
greedtn <naoto.tsuda63@gmail.com>
2021-04-15 01:15:01 +0900

    extra 20/20

--- 第2回/c.py
+++ 第2回/c.py
@@ -3,16 +3,21 @@ A = list(map(int, input().split()))
 Q = int(input())
 MOD = 998244353
 
-zero_list = []
+zero_list = [False] * (N)
 
 # s[i] : A1 ~ Ai までの積
 s = [1]
 for i in range(N):
     if A[i] == 0:
         s.append(s[i])
-        zero_list.append(i)
+        zero_list[i] = True
     else:
         s.append(s[i] * A[i] % MOD)
+
+zeros = [0]
+for i in range(N):
+    if zero_list[i]: zeros.append(zeros[i] + 1)
+    else: zeros.append(zeros[i])
 
 # s2[i] : s1 ~ si までの積
 s2 = [1]
@@ -21,18 +26,13 @@ for i in range(N):
 
 for i in range(Q):
     l, r = map(int, input().split())
-    ok = True
-    for zero in zero_list:
-        if l <= zero + 1 and zero + 1 <= r - 1:
-            print(0)
-            ok = False
-            break
-    if ok == False:
+    if zeros[l-1] != zeros[r-1]:
+        print(0)
         continue
     ans = 1
     ans *= pow(s[r-1], r-l, MOD)
-    ans %= MOD
-    ans *= s2[l-2]
+    if l != 1:
+        ans *= s2[l-2]
     ans %= MOD
     ans *= pow(s2[r-2], MOD - 2, MOD)
     ans %= MOD
