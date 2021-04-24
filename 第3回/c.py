2059d45457ad2472b95b3e25a9754e045ebef5f3
greedtn <naoto.tsuda63@gmail.com>
2021-04-24 20:10:59 +0900

    extra 25/30

--- 第3回/c.py
+++ 第3回/c.py
@@ -1,21 +1,34 @@
-
+import sys
+import resource
+resource.setrlimit(resource.RLIMIT_STACK, (-1, -1)) # スタック領域を拡大
+sys.setrecursionlimit(1000000) # 再帰回数の上限を拡大
 
 S = input()
 N = int(input())
 
-# ansを格納
-ans = [None] * (N+1)
-ans[0] = S
+plan = [0]
 
 for i in range(N):
-    plan = input().split()
-    if int(plan[0]) == 1:
-        ans[i+1] = plan[2]+ ans[int(plan[1])]
+    p = input().split()
+    plan.append(p)
+    # print(plan)
+
+def rec(p):
+    if p[0] == "1":
+        # 追加するときの操作
+        if p[1] == "0":
+            return p[2] + S
+        else:
+            return p[2] + rec(plan[int(p[1])])
     else:
-        ans[i+1] = ans[int(plan[1])][:-1]
+        # 削除するときの操作
+        if p[1] == "0":
+            return S[:-1]
+        else:
+            old = rec(plan[int(p[1])])
+            return old[:-1]
 
 Q = int(input())
 for _ in range(Q):
     q = int(input())
-    print(ans[q])
-
+    print(rec(plan[q]))
