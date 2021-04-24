7356dd5e6588aa02dc256f85db1926fac46c4ec8
greedtn <naoto.tsuda63@gmail.com>
2021-04-25 01:26:14 +0900

    Extra AC

--- 第3回/c.py
+++ 第3回/c.py
@@ -6,29 +6,54 @@ sys.setrecursionlimit(1000000) # 再帰回数の上限を拡大
 S = input()
 N = int(input())
 
+ans = [None for _ in range(N+1)]
 plan = [0]
 
 for i in range(N):
     p = input().split()
     plan.append(p)
-    # print(plan)
 
-def rec(p):
+Q = int(input())
+q_list = [False for _ in range(N+1)]
+q_ = []
+for _ in range(Q):
+    q = int(input())
+    q_list[q] = True
+    q_.append(q)
+
+def rec(p, q):
     if p[0] == "1":
         # 追加するときの操作
         if p[1] == "0":
-            return p[2] + S
+            if q_list[q]:
+                ans[q] = p[2] + S
+                return ans[q]
+            else:
+                return p[2] + S
         else:
-            return p[2] + rec(plan[int(p[1])])
+            if q_list[q]:
+                ans[q] = p[2] + rec(plan[int(p[1])], int(p[1]))
+                return ans[q]
+            else:
+                return p[2] + rec(plan[int(p[1])], int(p[1]))
     else:
         # 削除するときの操作
         if p[1] == "0":
-            return S[:-1]
+            if q_list[q]:
+                ans[q] = S[:-1]
+                return ans[q]
+            else:
+                return S[:-1]
         else:
-            old = rec(plan[int(p[1])])
-            return old[:-1]
+            old = rec(plan[int(p[1])], int(p[1]))
+            if q_list[q]:
+                ans[q] = old[:-1]
+                return ans[q]
+            else:
+                return old[:-1]
 
-Q = int(input())
-for _ in range(Q):
-    q = int(input())
-    print(rec(plan[q]))
+for q in q_:
+    if ans[q] != None:
+        print(ans[q])
+    else:
+        print(rec(plan[q], q))
