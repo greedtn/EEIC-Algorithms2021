d52a7a7f68f3aadad2514e53cc2d8d7520ec7608
greedtn <naoto.tsuda63@gmail.com>
2021-05-29 21:38:37 +0900

    第7回終わり

---
+++ 第7回/b.py
@@ -0,0 +1,18 @@
+N, S = map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = [[False for _ in range(S+1)] for _ in range(N+1)]
+
+dp[0][0] = True
+
+for i in range(1, N+1):
+    for j in range(S+1):
+        if j - A[i-1] >= 0:
+            dp[i][j] = dp[i-1][j] or dp[i-1][j-A[i-1]]
+        else:
+            dp[i][j] = dp[i-1][j]
+
+if dp[N][S]:
+    print("Yes")
+else:
+    print("No")
--- 第7回/c.py
+++ 第7回/c.py
@@ -1,13 +1,37 @@
-N = int(input())
-h = list(map(int, input().split()))
+N, W = map(int, input().split())
+vw = [list(map(int, input().split())) for _ in range(N)]
+# vwをvでソート（昇順）
+vw.sort()
+INF = 10010010010
+# DPテーブル
+dp_b = [[[INF for _ in range(N+1)] for _ in range(W+1)] for _ in range(N+1)]
+# 初期化
+for i in range(N+1):
+    for j in range(W+1):
+        dp_b[i][j][0] = 0
+for i in range(N):
+    for j in range(W+1):
+        for k in range(N+1):
+            # 重さがj以下のとき
+            if vw[i][1] <= j and k > 0:
+                dp_b[i+1][j][k] = min(dp_b[i][j][k], dp_b[i][j-vw[i][1]][k-1] + vw[i][0])
+            else:
+                dp_b[i+1][j][k] = dp_b[i][j][k]
+# 降順にソート
+vw.reverse()
+dp_a = [[[0 for _ in range(N+1)] for _ in range(W+1)] for _ in range(N+1)]
+for i in range(N):
+    for j in range(W+1):
+        for k in range(N+1):
+            # 重さがj以下のとき
+            if vw[i][1] <= j and k > 0:
+                dp_a[i+1][j][k] = max(dp_a[i][j][k], dp_a[i][j-vw[i][1]][k-1] + vw[i][0])
+            else:
+                dp_a[i+1][j][k] = dp_a[i][j][k]
 
-INF = 1001001001
-dp = [INF] * N
-dp[0] = 0
-
-for i in range(N-1):
-    dp[i+1] = min(dp[i] + abs(h[i] - h[i+1]))
-    if i < N - 2:
-        dp[i+2] = min(dp[i] + abs(h[i] - h[i+2]))
-
-print(dp[N-1])
+ans = -1
+for i in range(N+1):
+    for j in range(W+1):
+        for k in range(1, N+1):
+            ans = max(ans, dp_a[i][j][k] - dp_b[N-i][W-j][k-1])
+print(ans)
