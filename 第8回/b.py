2a92b63e886479efe954194a467b4ec9dae75e12
greedtn <naoto.tsuda63@gmail.com>
2021-06-13 05:44:08 +0900

    基本問題2をちゃんと理解

--- 第8回/b.py
+++ 第8回/b.py
@@ -6,11 +6,12 @@ MOD = 10**9 + 7
 dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
 
 dp[0][0] = 1
-for i in range(1, N+1):
-    cum = [0 for _ in range(K+1)]
-    for j in range(K+1+1):
-        cum[j] = (cum[j-1] + dp[i-1][j-1]) % MOD
+
+for i in range(N):
+    s = [0]
     for j in range(K+1):
-        dp[i][j] = (cum[j+1] - cum[max(0, j-A[i])]) % MOD
+        s.append((s[-1] + dp[i][j]) % MOD)
+    for j in range(K+1):
+        dp[i+1][j] = (s[j+1] - s[max(0, j - A[i])]) % MOD
 
 print(dp[N][K])
